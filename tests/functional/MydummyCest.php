ac589671566f33b3290c40c6d97a70852eab4dff
insolita <webmaster100500@ya.ru>
2017-05-25 07:36:47 +0800

    fix cest

--- tests/functional/MydummyCest.php
+++ tests/functional/MydummyCest.php
@@ -12,7 +12,7 @@ class MydummyCest
     public function checkOpen(FunctionalTester $I)
     {
         $I->wantToTest('CI Functional module test work');
-        $I->amOnPage('mydummy/default/index');
+        $I->amOnRoute('mydummy/default/index');
         $I->see('Success', 'h1');
         $I->see('Dummy Module', '#data');
     }
