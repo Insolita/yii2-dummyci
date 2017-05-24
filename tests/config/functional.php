a4279e8315fbde10f92a7304bb625327e571ca88
insolita <webmaster100500@ya.ru>
2017-05-25 07:09:32 +0800

    add url config

--- tests/config/functional.php
+++ tests/config/functional.php
@@ -1,4 +1,13 @@
 <?php
 $base = require(__DIR__).'/base.php';
-$functional = [];
+$functional = [
+    'components'=>[
+        'urlManager'=>[
+            'class'=>\yii\web\UrlManager::class,
+            'showScriptName' => true,
+            'enablePrettyUrl' => false,
+            'baseUrl' => 'http://localhost:8080'
+        ]
+    ]
+];
 return \yii\helpers\ArrayHelper::merge($base, $functional);
